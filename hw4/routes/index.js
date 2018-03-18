11f1ff37204c83c0e0819d6376c67af6722cc183
Lauren Cheong <lauren.cheong@stonybrook.edu>
2018-03-18 00:43:15 -0400

    added multer

--- hw4/routes/index.js
+++ hw4/routes/index.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var cassandra = require('cassandra-driver');
+var async = require('async');
+var client = new cassandra.Client({contactPoints: ['localhost'], keyspace: 'hw4'});
+
+var multer = require('multer');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.post('/deposit', function(req, res){
-  res.send(req.body.filename);
+  // depositFiles(req.body.filename, req.file, function(err, ret){
+
+  // });
+  res.send(req.file);
 });
 
+function depositFiles(filename, contents, callback){
+  // client.execute("INSERT INTO imgs (")
+}
 module.exports = router;
